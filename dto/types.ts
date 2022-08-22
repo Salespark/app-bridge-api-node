156195f94cbfb86f1baa9362bfb0ad835cc2f832
Khakan Ali <khakan.ali@alpharages.com>
2022-08-23 02:33:32 +0500

    add aggregate method with response dto

---
+++ dto/types.ts
@@ -0,0 +1,5 @@
+export type ResponseDto = {
+    statusCode: number;
+    message: string;
+    data: any;
+}
--- types/resources/cart.d.ts
+++ types/resources/cart.d.ts
@@ -1,20 +1,33 @@
 /**
- * Get the cart from the given marketplace
- *
- * @param {String} cartId - ID of the cart
- * @param {String} marketplaceId - Marketplace id
- * @return {Promise} The fetched cart from the marketplace
- * */
-export function get(cartId: string, marketplaceId: string): Promise<any>;
-/**
  * Get the list of carts form the given marketplace
  *
  * @param {Object} data - Data to fetch the relevant carts
  * @param {String} data.marketplaceId - ID of the marketplace
  * @param {Object} data.payload - Data payload
- * @return {Promise} List of carts
+ * @return {Promise<ResponseDto>} List of carts
  * */
 export function list(data: {
     marketplaceId: string;
     payload: any;
-}): Promise<any>;
+}): Promise<ResponseDto>;
+/**
+ * Get the cart from the given marketplace
+ *
+ * @param {String} cartId - ID of the cart
+ * @param {String} marketplaceId - Marketplace id
+ * @return {Promise<ResponseDto>} The fetched cart from the marketplace
+ * */
+export function get(cartId: string, marketplaceId: string): Promise<ResponseDto>;
+/**
+ * Get the cart aggregation from the given marketplace
+ *
+ * @param {Object} payload - Data to fetch the cart aggregation
+ * @param {String} payload.marketplaceId - Marketplace id
+ * @param {String} payload.query - Mongodb aggregate query(JSON.stringify)
+ * @return {Promise<ResponseDto>} The fetched cart aggregation from the marketplace
+ * */
+export function aggregate(payload: {
+    marketplaceId: string;
+    query: string;
+}): Promise<ResponseDto>;
+import { ResponseDto } from "../dto/types";
